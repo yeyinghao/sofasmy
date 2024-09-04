d8a2b7209cbea4a5d52328add1ea4cd578fa763a
yeyinghao <1113531030@qq.com>
2024-09-04 23:55:54 +0800

    初始化

--- app/core/model/src/main/java/com/luman/sofasmy/model/user/User.java
+++ app/core/model/src/main/java/com/luman/sofasmy/model/user/User.java
@@ -1,6 +1,7 @@
 package com.luman.sofasmy.model.user;
 
 import com.luman.sofa.common.dal.model.DP;
+import com.luman.sofasmy.dto.user.enums.SexEnum;
 import lombok.Data;
 
 @Data
@@ -8,4 +9,5 @@ public class User extends DP {
 
 	private String username;
 
+	private SexEnum sex;
 }
